8f89550fc7f881cdea21882c9a108ffed9b1a3b8
jude-snyder <judesnyder10@gmail.com>
2026-03-09 17:32:55 +0000

    Working on tests...

---
+++ index.spec.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect, mock } from "bun:test";
+import * as weather from "./get-current";
+ 
+describe("index script", () => {
+
+    it("should log the city the user lives in")
+})
