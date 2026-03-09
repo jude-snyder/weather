6a2369f82b2c9b37a625c97562be1892dc72c36a
jude-snyder <judesnyder10@gmail.com>
2026-03-09 18:28:25 +0000

    I got the spec.ts tests made!

--- index.spec.ts
+++ index.spec.ts
@@ -1,7 +1,61 @@
 import { describe, it, expect, mock } from "bun:test";
-import * as weather from "./get-current";
- 
-describe("index script", () => {
+describe ("index script tests", () => {
+    it ("should log the city the user lives in", async () => {
+        const logMock = mock();
+        console.log = logMock;
+        console.log(
+            "You are living in",
+            "Grand Rapids",
+            "bro 😎"
+        );
 
-    it("should log the city the user lives in")
-})
+        expect (logMock).toHaveBeenCalledWith (
+            "You are living in",
+            "Grand Rapids",
+            "bro 😎"
+        );
+    });
+
+    it ("should log the country of the location", async () => {
+        const logMock = mock();
+        console.log = logMock;
+        console.log(
+            "Your country is the",
+            "United States",
+            ". Isn't that obvious? 🤔"
+        );
+    });
+
+    it ("should display the current local time", async () => {
+        const logMock = mock();
+        console.log = logMock;
+        console.log(
+            "Here is the date of today, plus the current time! 😄",
+            "2026-03-09 12:45"
+        );
+        expect(logMock).toHaveBeenCalled();
+    });
+
+      it ("should print the real temperature in degrees", async () => {
+        const logMock = mock();
+        console.log = logMock;
+        console.log("Here's the real temperature:", 45, "degrees.");
+        expect(logMock).toHaveBeenCalled();
+    });
+
+
+      it ("should print the feels-like temperature", async () => {
+        const logMock = mock();
+        console.log = logMock;
+        console.log("And here's the temp that it really feels like 💀", 40, "degrees!");
+        expect(logMock).toHaveBeenCalled();
+      });
+
+
+      it ("should print the wind speed", async () => {
+        const logMock = mock();
+        console.log = logMock;
+        console.log("Here's the wind speed.", 12);
+        expect(logMock).toHaveBeenCalled();
+    })
+});
